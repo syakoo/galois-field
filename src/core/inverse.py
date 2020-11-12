3259788836f755460c6c94d1d301359bba6c19bc
syakoo <nichijou4009@gmail.com>
2020-11-12 23:26:43 +0900

    add: a doc to inverse_poly().

--- src/core/inverse.py
+++ src/core/inverse.py
@@ -18,8 +18,17 @@ def inverse_el(el: Fp, p: int) -> Fp:
 
 
 def inverse_poly(poly: np.poly1d, p: int, mod_poly: np.poly1d) -> np.poly1d:
+    """Compute the inverse of an element over Fpn.
+
+    Args:
+        poly (np.poly1d): An element in Fpn
+        p (int): A prime number.
+        mod_poly (np.poly1d): An monic irreducible polynomial.
+
+    Returns:
+        np.poly1d: The inverse of the element over Fpn.
+    """
     if len(poly.coeffs) == 1:
-        print(poly.coeffs, type(poly.coeffs[0]))
         inverse_of_el = inverse_el(poly.coeffs[0], p)
         return np.poly1d(np.array([inverse_of_el]))
 
