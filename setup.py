38f537759ee86325e3a497744ca3ec4c4e798258
sako data <sakoooo1001@gmail.com>
2020-11-14 20:16:48 +0900

    update: version to v2.0.0 🐍

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def _requires_from_file(filename):
 
 setuptools.setup(
     name="galois_field",
-    version="1.0.2",
+    version="2.0.0",
     author="Sakoda Takuya",
     author_email="sakodata0318@gmail.com",
     description="Galois Field: GF(p^n)",
