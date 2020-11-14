21c9e59299a9c507e249eab3b507b3c3174f1487
syakoo <nichijou4009@gmail.com>
2020-11-14 10:17:48 +0900

    add: a doc of GFp class.

--- galois_field/GFp.py
+++ galois_field/GFp.py
@@ -4,12 +4,26 @@ from .ElementInGFp import ElementInGFp
 
 
 class GFp:
-    def __init__(self, p: int):
-        """Galois Field: GF(p)
+    """Galois Field: GF(p)
 
-        Args:
-            p (int): A prime number.
-        """
+    Args:
+        p (int): A prime number.
+
+    Examples:
+        >>> from galois_field import GFp
+
+        In this case, p = 11.
+        >>> gf = GFp(11)
+
+        Generate the element in GF(11).
+        >>> gf.elm(5) # 5 (mod 11)
+        ElementInGFp(5, 11)
+
+        >>> gf.elm(13) # 2 (mod 11)
+        ElementInGFp(2, 11)
+    """
+
+    def __init__(self, p: int):
         self.__p = p
 
     @property
