a11876b2fb8ceca3b2affc9e52a9789a234909cd
syakoo <nichijou4009@gmail.com>
2020-11-28 21:05:28 +0900

    add: prime number decision.

--- galois_field/core/validator.py
+++ galois_field/core/validator.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 from . import gcd, modulus
@@ -22,6 +24,29 @@ def is_irreducible_poly(poly: np.poly1d, p: int) -> bool:
         gcd_poly = gcd.gcd_poly(
             poly, modulus.modulus_poly_over_fp(gx + np.poly1d([p-1, 0]), p), p)
         if len(gcd_poly.coeffs) != 1:
+            return False
+
+    return True
+
+
+def is_prime(value: int) -> bool:
+    """Detect whether a value is prime or not.
+
+    Args:
+        value (int): value.
+
+    Returns:
+        bool: Is the value prime.
+    """
+    if value < 2:
+        return False
+    if value == 2:
+        return True
+    if value % 2 == 0:
+        return False
+
+    for k in range(3, int(math.sqrt(value)) + 1, 2):
+        if value % k == 0:
             return False
 
     return True
